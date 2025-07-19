6fb206b347cb2edd4bd854196bf10014c4f3656a
omer-sm <145832643+omer-sm@users.noreply.github.com>
2025-07-19 13:04:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dov-analytics/'
+  base: '/dov-analytics-front/'
 })
